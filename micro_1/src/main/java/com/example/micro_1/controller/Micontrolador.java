fc6ea6234ad5561286c0ead01239fd87bec7b8b6
juan <jugom123@gmail.com>
2021-06-03 16:52:41 +0200

    docker

---
+++ micro_1/src/main/java/com/example/micro_1/controller/Micontrolador.java
@@ -0,0 +1,14 @@
+package com.example.micro_1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Micontrolador {
+	
+	@GetMapping("/")
+	public String hello() {
+		return "hello";
+	}
+
+}
